ee71fca737a058d61aacafb16ff3bcc0124499be
Lindell Carter <lindellcarternyc@gmail.com>
2018-04-04 23:52:44 -0400

    added hero and nav to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,45 @@
 import * as React from 'react'
 import './App.css'
 
-const logo = require('./logo.svg')
+const Headshot1 = require('./assets/images/lindell_picture.jpg')
+
+const Hero = (): JSX.Element => {
+  return (
+    <div 
+      style={{
+        background: `url(${Headshot1})`,
+        backgroundSize: 'cover',
+        width: '100%',
+        maxWidth: '800px',
+        height: '100vh',
+        maxHeight: '500px',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center center',
+        margin: '0 auto'
+      }}
+    />
+  )
+}
+
+const Navigation = (): JSX.Element => {
+  return (
+    <nav>
+      <ul
+        style={{border: '1px solid red'}}
+      >
+        <li>Home / News</li>
+        <li>Calendar</li>
+      </ul>
+    </nav>
+  )
+}
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Hero />
+        <Navigation />
       </div>
     )
   }
