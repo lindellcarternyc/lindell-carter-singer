2915ec5ac0c85e06fb00d0626e5679c63e832a68
Lindell Carter <lindellcarternyc@gmail.com>
2018-04-06 17:05:59 -0400

    created new hoverable HOC

---
+++ src/components/Hoverable.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react'
+
+export function makeHoverableComponent<ComponentProps extends { active?: boolean }>(
+  Component: React.ComponentType<ComponentProps>
+) {
+  interface HoverableComponentState {
+    isHovered: boolean
+  }
+  class HoverableComponent extends React.Component<ComponentProps, HoverableComponentState> {
+    constructor(props: ComponentProps) {
+      super(props)
+
+      this.state = { isHovered: false }
+    }
+
+    render() {
+      const active: boolean = this.props.active === true || this.state.isHovered
+      let props: ComponentProps = this.props
+      return (
+        <div
+          onMouseOver={this._onMouseOver}
+          onMouseOut={this._onMouseOut}
+        >
+          <Component
+            {...props}
+            active={active} 
+          />
+        </div>
+      )
+    }
+
+    private _onMouseOver = () => {
+      this.setState({ isHovered: true })
+    }
+    private _onMouseOut = () => {
+      this.setState({ isHovered: false })
+    }
+  }
+
+  return HoverableComponent
+}
+
+export default makeHoverableComponent
