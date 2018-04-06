2a4e014201ac21f0986738afd091411850a2bc40
Lindell Carter <lindellcarternyc@gmail.com>
2018-04-06 17:07:16 -0400

    added calendar and mock data to dummy app

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,54 @@ const Headshot1 = require('./assets/images/lindell_picture.jpg')
 
 import Navigation from './components/navigation/Navigation'
 import Hero from './components/Hero'
-import ContactForm from './components/ContactForm'
+// import ContactForm from './components/ContactForm'
 import PageTitle from './components/PageTitle'
+import Calendar from './components/calendar/Calendar'
+
+import { MonthData } from './components/calendar/types'
+const UpcomingEvents: MonthData[] = [
+  {
+    month: 'April',
+    year: '2018',
+    events: [
+      {
+        dates: ['04.13', '04.14'],
+        title: 'Martina Arroyo Puccini Concert',
+        location: 'Lang Recital Hall'
+      }
+    ]
+  },
+  {
+    month: 'May',
+    year: '2018',
+    events: [
+      {
+        dates: ['05.08', '05.11', '05.19'],
+        title: 'Verdi "AIDA" Radames',
+        location: 'Our Lady of Perpetual Help'
+      },
+      {
+        dates: ['05.25'],
+        title: 'Lindell Carter in Recital',
+        location: 'Opera America'
+      }
+    ]
+  }
+]
+
+const PastEvents: MonthData[] = [
+  {
+    month: 'March',
+    year: '2018',
+    events: [
+      {
+        dates: ['03.25'],
+        title: '"Madama Butterfly" - Pinkerton',
+        location: 'Regina Opera'
+      }
+    ]
+  }
+]
 
 class App extends React.Component {
   render() {
@@ -15,7 +61,11 @@ class App extends React.Component {
         <Hero image={Headshot1}/>
         <Navigation />
         <PageTitle name="Home & News"/>
-        <ContactForm />
+        {/* <ContactForm /> */}
+        <Calendar 
+          upcomingEvents={UpcomingEvents}
+          pastEvents={PastEvents}
+        />
       </div>
     )
   }
