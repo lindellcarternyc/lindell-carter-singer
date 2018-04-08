3d2a71cab20e35fe754a3859c4ee9a3b90b2d4c3
Lindell Carter <lindellcarternyc@gmail.com>
2018-04-07 21:28:55 -0400

    switched to photo page

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,16 @@ import './App.css'
 
 const Headshot1 = require('./assets/images/lindell_picture.jpg')
 
-// import Navigation from './components/navigation/Navigation'
-// import Hero from './components/Hero'
-// import ContactForm from './components/ContactForm'
-// import PageTitle from './components/PageTitle'
-// import Calendar from './components/calendar/Calendar'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import ContactPage from './pages/ContactPage'
-// import HomePage from './pages/HomePage'
-// import Layout from './components/Layout'
+import PhotoGalleryPage from './pages/PhotoGalleryPage'
 
 class App extends React.Component {
   render() {
     return (
       <>
         <Header heroImage={Headshot1} />
-        <ContactPage />
+        <PhotoGalleryPage />
         <Footer />
       </>
     )
