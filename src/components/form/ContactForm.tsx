d58504c059dea123561ef0ddc74d6dea53efd61a
Lindell Carter <lindellcarternyc@gmail.com>
2018-04-10 13:03:52 -0400

    cleanup form added state

--- src/components/form/ContactForm.tsx
+++ src/components/form/ContactForm.tsx
@@ -3,59 +3,108 @@ import * as React from 'react'
 import { Color, } from '../../constants/styles'
 import { Breakpoint, makeResponsiveComponent } from '../utils/responsive'
 
-import Input from './Input'
+import Input from './components/Input'
+import SubmitButton from './components/SubmitButton'
 
 interface ContactFormProps {
   breakpoint?: Breakpoint
 }
-const ContactForm = (props: ContactFormProps): JSX.Element => {
-  const { breakpoint } = props
-  let displayStyles: {
-    display: 'block'
-  } | {
-    display: 'grid'
-    gridTemplateColumns: string
-    gridColumnGap: string
-  }
-  
-  if ( breakpoint !== undefined && breakpoint > Breakpoint.Phone ) {
-    displayStyles = {
-      display: 'grid',
-      gridTemplateColumns: 'repeat(2, 1fr)',
-      gridColumnGap: '1rem'
+interface ContactFormState {
+  name: string
+  email: string
+  message: string
+}
+
+class ContactForm extends React.Component<ContactFormProps, ContactFormState> {
+  constructor(props: ContactFormProps) {
+    super(props)
+
+    this.state = {
+      name: '',
+      email: '',
+      message: ''
     }
-  } else {
-    displayStyles = { display: 'block' }
   }
-  return (
-    <>
-      <form
-        style={{padding: '1rem', background: Color.White}}
-      >
-        <div
-          style={{
-            marginBottom: '1rem',
-            ...displayStyles
-          }}
+
+  onChange = (name: string, value: string) => {
+    switch (name) {
+      case 'name':
+        this.setState({ name: value })
+        break
+      case 'email':
+        this.setState({ email: value })
+        break
+      case 'message':
+        this.setState({ message: value })
+        break
+      default: return
+    }
+  }
+
+  onSubmit = (evt: React.SyntheticEvent<HTMLFormElement | HTMLButtonElement>) => {
+    evt.preventDefault()
+    evt.stopPropagation()
+    const data = { ...this.state }
+    console.dir(data)
+  }
+
+  render() {
+    return (
+      <>
+        <form
+          style={{ padding: '1rem', background: Color.White }}
+          onSubmit={this.onSubmit}
         >
-          <Input label="Name" />
-          <Input label="E-mail" type="email"/>
-          <Input label="Message" type="textarea" />
-        </div>
-        <button 
-          type="submit"
-          style={{
-            color: Color.White,
-            backgroundColor: Color.Scarlet,
-            border: 'none',
-            outline: 'none',
-            padding: '0.5rem 1.5rem'
-          }}
-        >SUBMIT
-        </button>
-      </form>
-    </>
-  )
+          <div
+            style={{
+              marginBottom: '1rem',
+              ...this._displayStyles
+            }}
+          >
+            <Input
+              label="Name"
+              onChange={this.onChange}
+              name="name"
+              value={this.state.name}
+            />
+            <Input
+              label="E-mail"
+              type="email"
+              onChange={this.onChange}
+              name="email"
+              value={this.state.email}
+            />
+            <Input
+              label="Message"
+              type="textarea"
+              onChange={this.onChange}
+              name="message"
+              value={this.state.message}
+            />
+          </div>
+          <SubmitButton disabled={!this._isValid} onClick={this.onSubmit} />
+        </form>
+      </>
+    )
+  }
+
+  private get _displayStyles() {
+    const { breakpoint } = this.props
+    if (breakpoint !== undefined && breakpoint > Breakpoint.Phone) {
+      return {
+        display: 'grid',
+        gridTemplateColumns: 'repeat(2, 1fr)',
+        gridColumnGap: '1rem'
+      }
+    } else {
+      return { display: 'block' }
+    }
+  }
+
+  private get _isValid(): boolean {
+    const { name, email, message } = this.state
+    return name !== '' && email !== '' && message !== ''
+  }
 }
 
 export default makeResponsiveComponent(ContactForm)
--- src/components/form/components/Input.tsx
+++ src/components/form/components/Input.tsx
@@ -1,27 +1,28 @@
 import * as React from 'react'
 
 import Label from './Label'
+import TextArea from './TextArea'
+import TextInput from './TextInput'
 
 interface InputProps {
   label: string
   type?: 'email' | 'textarea'
+  onChange: ( name: string, value: string ) => void
+  name: string
+  value: string
 }
 const Input = (props: InputProps): JSX.Element => {
   const style = {
     width: '100%',
-    padding: '0.75rem'
+    padding: '0.75rem',
+    fontSize: '0.75rem'
   }
+  const { label, type, ...rest } = props 
   const ipt = props.type === 'textarea' 
     ? (
-      <textarea 
-        rows={10}
-        style={style}
-      />
+      <TextArea style={style} {...rest} />
     ) : (
-      <input 
-        style={style}
-        type={props.type}
-      />
+      <TextInput style={style} {...rest} />
     )
   return (
     <div
