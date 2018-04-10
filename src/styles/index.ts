7d7adc10cd8a4d3ac4d1319410b9b3de49dd824b
Lindell Carter <lindellcarternyc@gmail.com>
2018-04-10 17:29:24 -0400

    styles

---
+++ src/styles/index.ts
@@ -0,0 +1,13 @@
+const BLUE = '#0582ca'
+const DARK_GREY = '#2e3438'
+const SCARLET = '#550527'
+const WHITE = 'white'
+const LIGHT_GREY = '#6c7b84'
+
+export const Color = {
+  BLUE,
+  DARK_GREY,
+  SCARLET,
+  WHITE,
+  LIGHT_GREY
+}
