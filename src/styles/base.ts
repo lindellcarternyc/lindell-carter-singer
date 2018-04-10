1f06ce3cc53e4493e7d66a4341d8ee067dc3705f
Lindell Carter <lindellcarternyc@gmail.com>
2018-04-10 17:09:26 -0400

    moved styles module

---
+++ src/styles/base.ts
@@ -0,0 +1,15 @@
+const BLUE =        '#0582ca'
+const DARK_GREY =   '#2e3438'
+const LIGHT_GREY =  '#6c7b84'
+const SCARLET =     '#550527'
+const WHITE =       'white'
+
+export const Color = {
+  BLUE,
+  DARK_GREY,
+  LIGHT_GREY,
+  SCARLET,
+  WHITE
+}
+
+export default { Color }
