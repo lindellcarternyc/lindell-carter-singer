e0137a52cf1482b2ef9275905414cef88dd6986f
Lindell Carter <lindellcarternyc@gmail.com>
2018-04-15 18:20:26 -0400

    refactored header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,52 @@
 import * as React from 'react'
 
-import Hero from './Hero'
-// import Navigation from './navigation'
+import { Route } from '../constants/routes'
+import { RouteComponentProps, withRouter } from 'react-router-dom'
+import { Breakpoint, makeResponsiveComponent } from './utils/responsive'
 
-interface HeaderProps {
-  heroImage: string
+import MobileNavigation from './mobile-navigation'
+import Title from './page/Title'
+import Navigation from './navigation'
+
+const titleForPathname = (pathname: string): string => {
+  if ( pathname === '/' ) { return 'Home & News'}
+  
+  let title = pathname.slice(1)
+  if ( title === 'media') {
+    return 'Watch & Listen'
+  }
+  return title.charAt(0).toUpperCase() + title.slice(1)
+}
+
+interface HeaderProps extends RouteComponentProps<{}> {
+  breakpoint?: Breakpoint
+  routes: Route[]
 }
 const Header = (props: HeaderProps): JSX.Element => {
+  const title = titleForPathname(props.location.pathname)
+
+  const breakpoint = props.breakpoint || Breakpoint.Phone
+  if ( breakpoint >= Breakpoint.Desktop) {
+    return (
+      <div 
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Title text={title} />
+        <Navigation routes={props.routes}/>
+      </div>
+    )
+  }
   return (
-    <div 
-      style={{
-        position: 'relative',
-        zIndex: -1
-      }}
-    >
-      <Hero image={props.heroImage} />
-      {/* <Navigation /> */}
-    </div>
+    <>
+      <MobileNavigation routes={props.routes}/>
+      <Title text={title} />
+    </>
   )
 }
 
-export default Header
+const ResponsiveHeader = makeResponsiveComponent(Header)
+
+export default withRouter(ResponsiveHeader)
